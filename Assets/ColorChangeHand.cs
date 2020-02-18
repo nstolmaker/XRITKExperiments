5dc3ca3e5809fa5547fadc71711472d35aeaf63b
Noah Stolmaker <nstolmaker@gmail.com>
2020-02-18 10:08:42 -0800

    basic version kinda working. pretty cool

---
+++ Assets/ColorChangeHand.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class ColorChangeHand : MonoBehaviour
+{
+    public Material defaultMaterial = null;
+    public Material activeMaterial = null;
+
+    private MeshRenderer meshRenderer = null;
+    public XRGrabInteractable grabInteractable = null;
+
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+       // grabInteractable = GetComponent<XRGrabInteractable>();
+
+        grabInteractable.onSelectExit.AddListener(SetFirst);
+        grabInteractable.onSelectEnter.AddListener(SetSecond);
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractable.onSelectExit.RemoveListener(SetFirst);
+        grabInteractable.onSelectEnter.RemoveListener(SetSecond);
+    }
+
+    private void SetFirst(XRBaseInteractor interactor)
+    {
+        meshRenderer.material = defaultMaterial;
+    }
+
+    private void SetSecond(XRBaseInteractor intereactor)
+    {
+        meshRenderer.material = activeMaterial;
+    }
+}
---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material defaultMaterial  = null;
+    public Material activeMaterial = null;
+
+    private MeshRenderer meshRenderer = null;
+    private XRGrabInteractable grabInteractable = null;
+
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        grabInteractable.onSelectExit.AddListener(SetFirst);
+        grabInteractable.onSelectEnter.AddListener(SetSecond);
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractable.onSelectExit.RemoveListener(SetFirst);
+        grabInteractable.onSelectEnter.RemoveListener(SetSecond);
+    }
+
+    private void SetFirst(XRBaseInteractor interactor)
+    {
+        meshRenderer.material = defaultMaterial;
+    }
+
+    private void SetSecond(XRBaseInteractor intereactor)
+    {
+        meshRenderer.material = activeMaterial;
+    }
+}
