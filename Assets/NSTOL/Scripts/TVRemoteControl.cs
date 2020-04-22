54457155eb6e60142fd8c113f983c360c3bcdb97
Noah Stolmaker <nstolmaker@gmail.com>
2020-04-22 14:38:52 -0700

    video playback is all programatic now, including querying for the dimensions. next up is using some kind of stream, and syncing frame with a custom Model via NORMAL

--- Assets/NSTOL/Scripts/TVRemoteControl.cs
+++ Assets/NSTOL/Scripts/TVRemoteControl.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Video;
 
 public class TVRemoteControl : MonoBehaviour
 {
+    [SerializeField]
     private XRController controller;    // name it "RightHand Controller"
+    [SerializeField]
     private XRRayInteractor interactor = null;
 
+    [SerializeField]
     private GameObject tv;  // name it "TVScreen"
+    [SerializeField]
+    private VideoPlayer videoPlayer;
 
+    [SerializeField]
     public Texture m_MainTexture, m_Normal, m_Albedo;
+    [SerializeField]
     private Renderer m_Renderer;
+
+    public string videoURL = "http://mirrors.standaloneinstaller.com/video-sample/Panasonic_HDC_TM_700_P_50i.mp4";
+
+    public bool tvReady = false;
+
 
     void Start()
     {
@@ -36,54 +50,65 @@ public class TVRemoteControl : MonoBehaviour
             throw new System.Exception("TV or Controller not found. TVRemoteControl failed to start. Check the TVRemoteControl.cs file and confirm you didn't rename the game objects");
         }
 
-        SetupTV();
-        //        StartCoroutine(GetMovieTexture());
-
+        CheckDimensions(videoURL);
     }
 
-    /*IEnumerator GetMovieTexture()
-    {
-        using (var uwr = UnityWebRequestMultimedia.GetMovieTexture("http://myserver.com/mymovie.ogv"))
-        {
-            yield return uwr.SendWebRequest();
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                Debug.LogError(uwr.error);
-                yield break;
-            }
-
-            MovieTexture movie = DownloadHandlerMovieTexture.GetContent(uwr);
-
-            GetComponent<Renderer>().material.mainTexture = movie;
-            movie.loop = true;
-            movie.Play();
-        }
-    }
-    */
     // Update is called once per frame
     void Update()
     {
-        CheckForTVCommands();
+        if (tvReady) 
+            CheckForTVCommands();
+
+
     }
 
-    void SetupTV()
+    /* 
+     * I stole this code from the internet and only modified it slightly. i'm suspicious of whether it loads the entire video or not in order to do this test.
+     * */
+    void CheckDimensions(string url)
     {
+        GameObject tempVideo = new GameObject("Temp video for " + url);
+        VideoPlayer videoPlayer = tempVideo.AddComponent<VideoPlayer>();
+        videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+        videoPlayer.targetTexture = new RenderTexture(1, 1, 0);
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = url;
+        videoPlayer.prepareCompleted += (VideoPlayer source) =>
+        {
+            DebugHelpers.Log("dimensions " + source.texture.width + " x " + source.texture.height); 
+            Debug.Log("dimensions " + source.texture.width + " x " + source.texture.height);
+            SetupTV(source.texture.width, source.texture.height);
+            Destroy(tempVideo);
+        };
+        videoPlayer.Prepare();
+    }
+
+    /* 
+     * Called by CheckDimensions when its Prepare() step is complete 
+     */
+    void SetupTV(int remoteVidWidth, int remoteVidHeight)
+    {
+
+
+
         // first MeshRenderer
         m_Renderer = tv.AddComponent<MeshRenderer>();
-       //m_Renderer = GetComponent<Renderer>();
 
         // then make the video texture
+        Texture2D VideoTextureOTF = new Texture2D(remoteVidWidth, remoteVidHeight, TextureFormat.RGBA32, false);   // set to video with and height
 
         // now make the material
         Material material = new Material(Shader.Find("Standard"));
-        AssetDatabase.CreateAsset(material, "Assets/VideoMaterialOTF.mat");
-        material.SetTexture("VideoTexture", m_Albedo);
+        //This will save the new object to the project if we want it to: AssetDatabase.CreateAsset(material, "Assets/VideoMaterialOTF.mat");
+
+        // now assign the texture to the material
+        material.SetTexture("VideoTextureOTF", m_Albedo);
 
         // asign the new material to our renderer
         m_Renderer.material = material;
 
-// now make the videoPlayer
-var videoPlayer = tv.AddComponent<UnityEngine.Video.VideoPlayer>();
+        // now make the videoPlayer
+        videoPlayer = tv.AddComponent<UnityEngine.Video.VideoPlayer>();
         var audioSource = gameObject.AddComponent<AudioSource>();
 
         //videoPlayer.clip = videoClip;
@@ -93,7 +118,11 @@ var videoPlayer = tv.AddComponent<UnityEngine.Video.VideoPlayer>();
         videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
         videoPlayer.SetTargetAudioSource(0, audioSource);
         videoPlayer.isLooping = true;
-        videoPlayer.url = "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_1280_10MG.mp4";
+        videoPlayer.url = videoURL;
+        videoPlayer.skipOnDrop = true;
+
+        tvReady = true;
+
     }
 
     public void CheckForTVCommands()
@@ -104,6 +133,7 @@ var videoPlayer = tv.AddComponent<UnityEngine.Video.VideoPlayer>();
         {
             if (trigger)
             {
+                DebugHelpers.Log("Height/Width: " + videoPlayer.height + " / " + videoPlayer.width);
                 DebugHelpers.Log("Primary A Button Capacitive touch: " + trigger);
                 if (tv.GetComponent<UnityEngine.Video.VideoPlayer>().isPlaying == true)
                 {
@@ -118,10 +148,10 @@ var videoPlayer = tv.AddComponent<UnityEngine.Video.VideoPlayer>();
 
     public void SwapVideo()
     {
-        // change aspect ratio of texture VideoTexture
+        // x change aspect ratio of texture VideoTexture
         // change video clip in VideoPlayer component
-        // make sure VideoMaterial's Rendering Maps are set to the VideoTexture still
-        // make sure the VideoMaterial is still assigned to the material of MeshRenderer component
+        // X make sure VideoMaterial's Rendering Maps are set to the VideoTexture still
+        // X make sure the VideoMaterial is still assigned to the material of MeshRenderer component
     }
 
     //
