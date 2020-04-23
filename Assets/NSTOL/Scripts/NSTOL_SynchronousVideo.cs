b36656a4704552ffedbf0eed058c595b9116380d
Noah Stolmaker <nstolmaker@gmail.com>
2020-04-22 20:07:14 -0700

    adding current video to the Normal remote sync stuff

---
+++ Assets/NSTOL/Scripts/NSTOL_SynchronousVideo.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.XR.Interaction.Toolkit;
+using Normal.Realtime;
+
+public class NSTOL_SynchronousVideo : RealtimeComponent
+{
+    [SerializeField]
+    private NSTOL_SynchronousVideoModel _model;
+    [SerializeField]
+    private GameObject tv;
+    void Start()
+    {
+        if (!tv)
+        {
+            tv = GameObject.Find("TVScreen");
+        }
+    }
+
+    private NSTOL_SynchronousVideoModel model
+    {
+        set
+        {
+            if (_model != null)
+            {
+                _model.frameNumberDidChange -= FrameNumberDidChange;
+                _model.playbackURLDidChange -= VideoURLDidChange;
+            }
+
+            _model = value;
+
+            if (_model != null)
+            {
+                _model.frameNumberDidChange += FrameNumberDidChange;
+                _model.playbackURLDidChange += VideoURLDidChange;
+            }
+        }
+    }
+
+
+    public void FrameNumberDidChange(NSTOL_SynchronousVideoModel model, int frameNumber)
+    {
+        SmoothToNewFrame(frameNumber);
+    }
+
+    private void SmoothToNewFrame(int frameNumber)
+    {
+        DebugHelpers.Log("Smoothing to new frame " + frameNumber);
+
+    }
+
+    public void SetFrame(int frameNumber)
+    {
+        _model.frameNumber = frameNumber;
+    }
+
+
+    public void VideoURLDidChange(NSTOL_SynchronousVideoModel model, string videoURL)
+    {
+        // update local video url to new url
+        DebugHelpers.Log("Loading new video URL: " + videoURL);
+        tv.GetComponent<VideoPlayer>().url = videoURL;
+    }
+
+    public void SetVideoURL(string videoURL)
+    {
+        _model.playbackURL = videoURL;
+    }
+}
---
+++ Assets/NSTOL/Scripts/NSTOL_SynchronousVideoTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NSTOL_SynchronousVideoTest : MonoBehaviour
+{
+    [SerializeField]
+    private string _playbackURL;
+    [SerializeField]
+    private string _previousPlaybackURL;
+
+    private NSTOL_SynchronousVideo _synchronousVideo;
+
+    void Start()
+    {
+        // Get a reference to the color sync component.
+        _synchronousVideo = GetComponent<NSTOL_SynchronousVideo>();
+    }
+
+    void Update()
+    {
+        // If the color has changed (via the inspector), call SetColor on the color sync component.
+        if (_playbackURL != _previousPlaybackURL)
+        {
+            _synchronousVideo.SetVideoURL(_playbackURL);
+        }
+    }
+}
