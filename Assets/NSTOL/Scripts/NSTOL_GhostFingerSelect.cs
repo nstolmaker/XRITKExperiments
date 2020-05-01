7925b00b98ab7cedc3f02bc1a9ab6921ebc4c8f6
Noah Stolmaker <nstolmaker@gmail.com>
2020-04-30 18:16:30 -0700

    more code cleanup. plus adding some haptics to the remote

--- Assets/NSTOL/Scripts/NSTOL_GhostFingerSelect.cs
+++ Assets/NSTOL/Scripts/NSTOL_GhostFingerSelect.cs
@@ -26,6 +26,12 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
     [SerializeField]
     private GameObject collidingWith;
 
+    [SerializeField]
+    private float hapticFingerHoverIntensity = 0.2f;
+
+
+    InputDevice device;
+    HapticCapabilities capabilities; // for storing the actual device. reduces lines of code for haptics.
 
     void Start()
     {
@@ -42,10 +48,8 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
         if (!ghostFinger)
         {
             ghostFinger = GameObject.Find("GhostFinger");
-            
-            //onCollisionEnter = (collider collider) => { }
-            //ghostFinger.transform.parent = thingManager.transform;
         }
+
         ghostFinger.GetComponent<NSTOL_GhostFingerClick>().CollideWithPlayer += (GameObject collideWithName) => collidingWith = collideWithName;
         ghostFinger.GetComponent<NSTOL_GhostFingerClick>().EndCollideWithPlayer += () => collidingWith = null; 
 
@@ -56,14 +60,9 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
 
     }
 
-    void Awake()
-    {
-
-    }
-
     private void SetHoldingRemote(XRBaseInteractor arg0)
     {
-        DebugHelpers.Log("SetHoldingRemote");
+        //DebugHelpers.Log("SetHoldingRemote");
         //interactor = controller.GetComponent<XRRayInteractor>();
         controller = arg0.GetComponent<XRController>();
         XRBaseInteractable remote = arg0.selectTarget;
@@ -71,12 +70,28 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
         //interactor.onSelectEnter.AddListener(PickUpPaddle);
         //interactor.onSelectExit.AddListener(DropPaddle);
         beingHeld = true;
+
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        if (device.TryGetHapticCapabilities(out capabilities))
+        {
+            if (!capabilities.supportsImpulse)
+            {
+                Debug.LogError("capabilities.supportsImpluse is false. Is this not a haptic-capable XR controller?");
+            }
+        }
     }
 
     private void SetNotHoldingRemote(XRBaseInteractor arg0)
     {
-        DebugHelpers.Log("SetNotHoldingRemote");
+        //DebugHelpers.Log("SetNotHoldingRemote");
         beingHeld = false;
+    }
+
+    private void SendHapticPulse()
+    {
+        uint channel = 0;
+        float duration = 0.05f;
+        device.SendHapticImpulse(channel, hapticFingerHoverIntensity, duration);
     }
 
     void Update()
@@ -100,40 +115,37 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
             // Joystick Click
             if (controller.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float trigger))
             {
-                if (trigger > 0.02)
+                // collidingWith is handled by the event system. It's instantiated in this file in the Start() method.
+                if (collidingWith != null)
                 {
-                    if (collidingWith != null)
+                    var tv = GameObject.Find("TVScreen");
+                    var syncVidComponent = tv.GetComponent<NSTOL_SynchronousVideo>();
+                    switch (collidingWith.name)
                     {
-                        //Debug.Log("thumbstickClick: " + trigger.ToString());
-                        // see if there's a collision and trigger an event if there is. (OnCollisionStay should be triggered, just set clicking=true and it'll run.)
-                        //DebugHelpers.Log("Clicking on: " + collidingWith.name);
-                        //Debug.LogError("Clicking on: " + collidingWith.name);
-                        var tv = GameObject.Find("TVScreen");
-                        var syncVidComponent = tv.GetComponent<NSTOL_SynchronousVideo>();
-                        switch (collidingWith.name)
-                        {
-                            case "Play":
-                                //DebugHelpers.Log("Play button pushed");
-                                //Debug.LogError("Play button pushed");
+                        case "Play":
+                            //DebugHelpers.Log("Play button mouseover");
+                            SendHapticPulse();
+                            if (trigger > 0.02) {
+                                DebugHelpers.Log("Play button pushed");
                                 syncVidComponent.Play();
-                                break;
-                            case "Pause":
-                                //DebugHelpers.Log("Pause button pushed");
-                                //Debug.LogError("Pause button pushed");
+                            }
+                            break;
+                        case "Pause":
+                            //DebugHelpers.Log("Pause button pushed");
+                            SendHapticPulse();
+                            if (trigger > 0.02)
                                 syncVidComponent.Pause();
-                                break;
-                            case "Stop":
-                                //DebugHelpers.Log("Stop button pushed");
-                                //Debug.LogError("Stop button pushed");
+                            break;
+                        case "Stop":
+                            //DebugHelpers.Log("Stop button pushed");
+                            SendHapticPulse();
+                            if (trigger > 0.02)
                                 syncVidComponent.Stop();
-                                break;
-                            default:
-                                //DebugHelpers.Log("DEFAULT ON OnCollisionStay" + collidingWith.name);
-                                //Debug.LogError("DEFAULT ON OnCollisionStay" + collidingWith.name);
-                                break;
-                        }
+                            break;
+                        default:
+                            //DebugHelpers.Log("DEFAULT ON OnCollisionStay" + collidingWith.name);
+                            break;
                     }
-
                 }
             }
         }
@@ -151,24 +163,5 @@ public class NSTOL_GhostFingerSelect : MonoBehaviour
         return returnVector;
     }
 
-    /*
-    void OnCollisionEnter(Collision collision)
-    {
-        collidingWith = collision.gameObject;
-        //if (collision.gameObject.name == "PrototypeRemoteButton1")
-        //{
-        if (clicking)
-        {
 
-            clicking = false;
-        }
-        //}
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        //collidingWith = null;
- 
-    }
-    */
 }
